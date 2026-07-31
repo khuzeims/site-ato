8c8be535c35858d7bdc6a8a7e910373d2e74276c
khuzeims <alitahaalbachar3@gmail.com>
2026-07-31 16:21:48 +0200

    Modification pages HTML

--- backend/creerAdmin.js
+++ backend/creerAdmin.js
@@ -1,5 +1,7 @@
 // Script à exécuter UNE SEULE FOIS pour créer le compte admin initial.
 // Usage : node creerAdmin.js
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 require("dotenv").config();
 const mongoose = require("mongoose");
