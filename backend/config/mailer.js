64937807bff3302c73de7ff4478cbcd619ebedf6
khuzeims <alitahaalbachar3@gmail.com>
2026-08-05 12:54:12 +0200

    Force Node DNS to prefer IPv4

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -5,7 +5,7 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "42.250.102.108",
     port: 587,
     secure: false,
     requireTLS: true,
