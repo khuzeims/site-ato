3be8fb152afe11a85df6457442adfc2ce3e85aab
khuzeims <alitahaalbachar3@gmail.com>
2026-08-05 12:26:39 +0200

    Force IPv4 for Gmail SMTP

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
-    family: 4,
+    lookup: (hostname, options, callback) => {
+        dns.lookup(hostname, { family: 4 }, callback);
+    },
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
