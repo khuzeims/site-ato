714cac6a37f51cba395868d8812e966ef6f16c0f
khuzeims <alitahaalbachar3@gmail.com>
2026-08-05 11:33:58 +0200

    Description des modifications

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
     },
+    family: 4, 
 });
 
 module.exports = transporter;
